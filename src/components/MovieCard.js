72e9a222f9608dd57c91f510985e83a9699aaaf7
kevgorix <dev.kevingomez@gmail.com>
2019-05-23 19:01:43 -0500

    Session 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,32 +1,53 @@
 import React from 'react';
 
 class MovieCard extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = { checked: false }
-    this.handleCheck = this.handleCheck.bind(this)
+  state = {
+    checked: false,
   }
 
-  handleCheck () {
+  handleCheck = () => {
     this.setState((state, props) => ({
       checked: !state.checked
-    }))
+    }), () => console.log('favourite added'))
   }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('get state from props')
+    if(props.genre === "horror") {
+      return {
+        checked: true,
+      }
+    }
+    return null
+  }
+
+  shouldComponentUpdate () {
+    //Remember to return true or false
+    return true
+  }
+
 
   componentDidMount () {
-    console.log(this.state)
+    //console.log(this.state)
   }
 
-  componentDidUpdate () {
-    console.log(this.state)
+  componentDidUpdate (prevProps, prevState) {
+    //console.log(this.state)
+  }
+
+  componentWillUnmount() {
+    console.log('I will unmount')
   }
   
   render() {
-    const { title, year, image, overview } = this.props
+    const { id, title, year, image, overview, deleteMovie } = this.props
     return <div className='movie-container'>   
         <div className='favourite-container'>
             <button onClick={this.handleCheck} className='favourite-button'>
                 <span className={`fa fa-star favourite-star ${this.state.checked ? 'checked': ''}`}></span>
+            </button>
+            <button onClick={() => deleteMovie(id)} className='close-button'>
+              <i className="fa fa-close"></i>
             </button>
         </div>
       <h1>{title}</h1>
@@ -37,6 +58,10 @@ class MovieCard extends React.Component {
       </div>
     </div>
     }
+}
+
+MovieCard.defaultProps = {
+  genre: 'comedia',
 }
 
 export default MovieCard
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,12 +3,26 @@ import moviesData from '../data/movies.json'
 import MovieCard from '../components/MovieCard'
 
 class Home extends React.Component {
+  state = {
+   ...moviesData,
+  }
+
+  deleteMovie = (movieId) => {
+    this.setState((state, props) => {
+      const movies = state.movies.filter((movie) => movie.id !== movieId)
+      return  {
+        movies
+      }
+    })
+  }
+
+
     render() {
-        const { movies } = moviesData
+        const { movies } = this.state
         return <div>
         <h1 className='main-title'>Movie App</h1>
         <div className='content'>
-          {movies.map((movie) => <MovieCard key={movie.id} {...movie} />)}
+          {movies.map((movie) => <MovieCard deleteMovie={this.deleteMovie} key={movie.id} {...movie} />)}
         </div>
       </div>
     }
